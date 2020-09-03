51fce7043bb03daa8382d2add9195b86f9a1c534
miguelneto0 <miguelneto0019@gmail.com>
2020-09-03 16:42:06 -0300

    Timeline concluida

---
+++ js/timeline.js
@@ -0,0 +1,62 @@
+// document.getElementById('los1').addEventListener('mousemove', geraspan)
+// document.getElementById('los2').addEventListener('mousemove', geraspan2)
+// document.getElementById('los3').addEventListener('mousemove', geraspan3)
+// document.getElementById('los4').addEventListener('mousemove', geraspan4)
+
+// // document.getElementById('circ1').addEventListener('mousemove', geraspancirc1)
+// // document.getElementById('circ2').addEventListener('mousemove', geraspancirc2)
+// // document.getElementById('circ3').addEventListener('mousemove', geraspancirc3)
+
+// function geraspan(){
+//     var spanlosan = document.getElementById('losan1')
+//     if (spanlosan.style.display === 'none')
+//         spanlosan.style.display = 'block'
+//     else
+//         spanlosan.style.display = 'none'
+// }
+
+// function geraspan2(){
+//     var spanlosan = document.getElementById('losan2')
+//     if (spanlosan.style.display === 'none')
+//         spanlosan.style.display = 'block'
+//     else
+//         spanlosan.style.display = 'none'
+// }
+
+// function geraspan3(){
+//     var spanlosan = document.getElementById('losan3')
+//     if (spanlosan.style.display === 'none')
+//         spanlosan.style.display = 'block'
+//     else
+//         spanlosan.style.display = 'none'
+// }
+
+// function geraspan4(){
+//     var spanlosan = document.getElementById('losan4')
+//     if (spanlosan.style.display === 'none')
+//         spanlosan.style.display = 'block'
+//     else
+//         spanlosan.style.display = 'none'
+// }
+// // function geraspan2(){
+// //     var spanlosan = document.getElementById('losan2')
+// //     if (spanlosan.style.display === 'none')
+// //         spanlosan.style.display = 'inline'
+// //     else
+// //         spanlosan.style.display = 'none'
+// // }
+
+// // function geraspan3(){
+// //     var spanlosan = document.getElementById('losan3')
+// //     if (spanlosan.style.display === 'none')
+// //         spanlosan.style.display = 'inline'
+// //     else
+// //         spanlosan.style.display = 'none'
+// // }
+// // function geraspan4(){
+// //     var spanlosan = document.getElementById('losan4')
+// //     if (spanlosan.style.display === 'none')
+// //         spanlosan.style.display = 'inline'
+// //     else
+// //         spanlosan.style.display = 'none'
+// // }
